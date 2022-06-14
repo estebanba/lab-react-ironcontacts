f9ce95d855039cc7408b85b8aac5616c23dec1b7
estebanba <esteban.ba@gmail.com>
2022-06-14 18:15:03 +0200

    do Iteration 6

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 import contactsJSON from "./contacts.json";
 
-const fiveContacts = contactsJSON.slice(0, 5);
-const restContacts = contactsJSON.slice(5);
+const allContacts = JSON.parse(JSON.stringify(contactsJSON));
+
+const fiveContacts = allContacts.slice(0, 5);
+// const restContacts = allContacts.slice(5);
 
 // "name": "Idris Elba",
 // "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
@@ -14,49 +16,73 @@ const restContacts = contactsJSON.slice(5);
 
 function App() {
   const [contacts, setContacts] = useState(fiveContacts);
+  // const [theRest, setRest] = useState(restContacts);
+  const handleRandom = () => {
+    let filterArray = contactsJSON.filter(
+      (celebrity) => !contacts.find(({ id }) => celebrity.id === id)
+    );
+    const randomContact =
+      filterArray[Math.round(Math.random() * filterArray.length)];
+    setContacts([...contacts, randomContact]);
+  };
 
   return (
     <div className="App">
-      <button
-        type="button"
-        onClick={() => {
-          const randomContact =
-            restContacts[Math.round(Math.random() * restContacts.length)];
-          const newContacts = JSON.parse(JSON.stringify(contacts));
-          newContacts.push(randomContact);
-          setContacts(newContacts);
-        }}
-      >
-        Add Random Contact
-      </button>
+      <div className="buttons">
+        {contacts.length > 0 && (
+          <button
+            type="button"
+            onClick={handleRandom}
 
-      <button
-        type="button"
-        onClick={() => {
-          const newContacts = JSON.parse(JSON.stringify(contacts));
-          newContacts.sort((first, second) =>
-            first.name.localeCompare(second.name)
-          );
-          setContacts(newContacts);
-        }}
-      >
-        Sort By Name
-      </button>
+            // () => {
+            // console.log("remaining contacts: ", theRest.length);
+            // const randomContact =
+            //   theRest[Math.round(Math.random() * theRest.length)];
 
-      <button
-        type="button"
-        onClick={() => {
-          const newContacts = JSON.parse(JSON.stringify(contacts));
-          newContacts.sort(
-            (first, second) => second.popularity - first.popularity
-          );
-          setContacts(newContacts);
-        }}
-      >
-        Sort By Popularity
-      </button>
+            // const newRest = JSON.parse(JSON.stringify(theRest));
+            // console.log(randomContact.name);
+            // const newFiltered = newRest.filter(
+            //   (toFilter) => toFilter.name !== randomContact.name
+            // );
 
-      <table>
+            // const newContacts = JSON.parse(JSON.stringify(contacts));
+            // newContacts.push(randomContact);
+
+            // setRest(newFiltered);
+            // setContacts(newContacts);
+            // }}
+          >
+            Add Random Contact
+          </button>
+        )}
+
+        <button
+          type="button"
+          onClick={() => {
+            const newContacts = JSON.parse(JSON.stringify(contacts));
+            newContacts.sort((first, second) =>
+              first.name.localeCompare(second.name)
+            );
+            setContacts(newContacts);
+          }}
+        >
+          Sort By Name
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            const newContacts = JSON.parse(JSON.stringify(contacts));
+            newContacts.sort(
+              (first, second) => second.popularity - first.popularity
+            );
+            setContacts(newContacts);
+          }}
+        >
+          Sort By Popularity
+        </button>
+      </div>
+      <table cellSpacing="0">
         <thead>
           <tr>
             <th>IronContacts</th>
@@ -69,6 +95,7 @@ function App() {
             <td>Popularity</td>
             <td>Won Oscar</td>
             <td>Won Emmy</td>
+            <td>Actions</td>
           </tr>
           {contacts.map((oneContact) => (
             <tr>
@@ -79,6 +106,21 @@ function App() {
               <td>{oneContact.popularity.toPrecision(4)}</td>
               {oneContact.wonOscar ? <td>🏆</td> : <td></td>}
               {oneContact.wonEmmy ? <td>🏆</td> : <td></td>}
+              <td>
+                <button
+                  type="button"
+                  onClick={() => {
+                    const newContacts = JSON.parse(JSON.stringify(contacts));
+                    console.log(oneContact.id, oneContact.name);
+                    const filteredArray = newContacts.filter(
+                      (toFilter) => toFilter.id !== oneContact.id
+                    );
+                    setContacts(filteredArray);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
